04b5be23ecd6f7f08ed4a172a46fda3772d2726c
danielmoessner <github@moesibert.de>
2021-10-18 14:24:17 +0200

    fix permissions

--- permissions.sh
+++ permissions.sh
@@ -1,8 +1,8 @@
-chown -R root:www-data /home/apps.de
-chmod -R 750 /home/apps.de
-find /home/apps.de -type f -print0|xargs -0 chmod 740
-chmod -R 770 /home/apps.de/tmp/media
-find /home/apps.de/tmp/media -type f -print0|xargs -0 chmod 760
-chmod 760 /home/apps.de/tmp/django.log
-chmod 770 /home/apps.de/tmp
-chmod -R 760 /home/apps.de/tmp/db.sqlite3
+chown -R root:www-data /home/thecornerhouse.de
+chmod -R 750 /home/thecornerhouse.de
+find /home/thecornerhouse.de -type f -print0|xargs -0 chmod 740
+chmod -R 770 /home/thecornerhouse.de/tmp/media
+find /home/thecornerhouse.de/tmp/media -type f -print0|xargs -0 chmod 760
+chmod 760 /home/thecornerhouse.de/tmp/django.log
+chmod 770 /home/thecornerhouse.de/tmp
+chmod -R 760 /home/thecornerhouse.de/tmp/db.sqlite3
